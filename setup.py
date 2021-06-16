3375b6351184b1dd07ddfd5d631846700dd11b85
The-Cloud-Dev <85949157+The-Cloud-Dev@users.noreply.github.com>
2021-06-16 16:14:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scratch2py",                     # This is the name of the package
-    version="0.0.1",                        # The initial release version
+    name="scratch2py", 
+    url="https://github.com/The-Cloud-Dev/scratch2py",                    
+    version="0.0.3",                        # The initial release version
     author="TheCloudDev",
     license="MIT",        
     description="Python to Scratch API connector",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(),    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
