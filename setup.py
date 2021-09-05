76a8de6ed2270a623679d6e4dddd91cd8ec78516
The-Cloud-Dev <85949157+The-Cloud-Dev@users.noreply.github.com>
2021-09-05 06:52:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 import setuptools
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scratch2py", 
-    url="https://github.com/The-Cloud-Dev/scratch2py",                    
-    version="0.1.8",                        
+    name="scratch2py",
+    url="https://github.com/The-Cloud-Dev/scratch2py",
+    version="0.2.9",
     author="TheCloudDev",
-    license="MIT",        
+    license="MIT",
     description="Python to Scratch API connector",
-    long_description=long_description,      
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      
-    python_requires='>=3.6',                 
-    install_requires=["websocket-client","wsaccel","ScratchEncoder"]                    
+    ],
+    python_requires='>=3.6',
+    install_requires=["websocket-client","ScratchEncoder", "requests"]
 )
