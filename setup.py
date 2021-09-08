f1149b4fc4104705370805d560efd04cc865987d
The-Cloud-Dev <85949157+The-Cloud-Dev@users.noreply.github.com>
2021-09-08 05:49:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="scratch2py",
     url="https://github.com/The-Cloud-Dev/scratch2py",
-    version="0.2.9",
+    version="0.3.1",
     author="TheCloudDev",
     license="MIT",
     description="Python to Scratch API connector",
